6bee95da376be97a2d02c12e4189ee4072e5dd62
Samuel Cochran <sj26@sj26.com>
2012-08-10 23:23:50 +0800

    Add Zipstream::Body spec

---
+++ spec/zipstream_body_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Zipstream::Body do
+  it "should stream as a body" do
+    stage = 1
+    body = Zipstream::Body.new do |zip|
+      stage = 2
+      zip.write "README", "This is a README!"
+      stage = 3
+      zip.write "LICENSE", "Copyright (c) 2012 Me"
+      stage = 4
+    end.to_enum
+
+    buffer = ""
+    stage.should == 1
+    buffer += body.next until stage > 1
+    stage.should == 2
+    buffer += body.next until stage > 2
+    stage.should == 3
+    loop { buffer += body.next }
+    stage.should == 4
+  end
+end
